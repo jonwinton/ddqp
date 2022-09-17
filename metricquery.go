4a77ae4f678a784167a96f21d4ed60ae3728c4a6
jonwinton <winton.jon@gmail.com>
2022-09-17 09:46:27 -0700

    add tests for metricquery

---
+++ metricquery.go
@@ -0,0 +1,40 @@
+package dotodag
+
+import "github.com/alecthomas/participle/v2/lexer"
+
+// https://docs.datadoghq.com/metrics/#anatomy-of-a-metric-query
+type MetricQuery struct {
+	Pos lexer.Position
+
+	Query []*Query `@@`
+}
+
+type Query struct {
+	Pos lexer.Position
+
+	Aggregator string      `@Ident ":"`
+	MetricName string      `@Ident( @"." @Ident)*`
+	Filters    []*Filter   `"{" ( @@ ( "," @@ )* )? "}"`
+	Function   []*Function `( @@ ( "." @@ )* )?`
+	By         string      `Ident`
+	Grouping   []string    `"{" ( @Ident ( "," @Ident )* )? "}"`
+}
+type Filter struct {
+	Key   string `@Ident ":"`
+	Value string `@(String|Ident)`
+}
+type Function struct {
+	Name string          `"." @Ident`
+	Args []*FunctionArgs `"(" ( @@ ( "," @@ )* )? ")"`
+}
+
+type Bool bool
+
+func (b *Bool) Capture(v []string) error { *b = v[0] == "true"; return nil }
+
+type FunctionArgs struct {
+	Boolean    *Bool    `  @("true"|"false")`
+	Identifier *string  `| @Ident ( @"." @Ident )*`
+	String     *string  `| @(String|Char|RawString)`
+	Number     *float64 `| @(Float|Int)`
+}
