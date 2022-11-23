249fddeffff762556cce282bc287a722bce987a0
jonwinton <winton.jon@gmail.com>
2022-11-23 07:56:56 -0800

    metric expression String() method init

--- metricexpression.go
+++ metricexpression.go
@@ -1,6 +1,7 @@
 package ddqp
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/alecthomas/participle/v2"
@@ -24,14 +25,13 @@ func (o *Operator) Capture(s []string) error {
 }
 
 type ExprValue struct {
-	Number        *float64     `  @(Float|Int)`
-	MetricQuery   *MetricQuery `| @@`
-	Subexpression *Expression  `| "(" @@ ")"`
+	Number        *float64          `  @(Float|Int)`
+	MetricQuery   *MetricQuery      `| @@`
+	Subexpression *MetricExpression `| "(" @@ ")"`
 }
 
 type Factor struct {
 	Base *ExprValue `@@`
-	// Exponent *ExprValue `( "^" @@ )?`
 }
 
 type OpFactor struct {
@@ -44,14 +44,19 @@ type Term struct {
 	Right []*OpFactor `@@*`
 }
 
+func (t *Term) GetQueries() []string {
+	queries := []string{t.Left.String()}
+
+	for _, v := range t.Right {
+		queries = append(queries, v.Factor.String())
+	}
+
+	return queries
+}
+
 type OpTerm struct {
 	Operator Operator `@("+" | "-")`
 	Term     *Term    `@@`
-}
-
-type Expression struct {
-	Left  *Term     `@@`
-	Right []*OpTerm `@@*`
 }
 
 type MetricExpression struct {
@@ -59,6 +64,26 @@ type MetricExpression struct {
 
 	Left  *Term     `@@`
 	Right []*OpTerm `@@*`
+}
+
+func (me *MetricExpression) GetQueries() map[string]string {
+	queries := me.Left.GetQueries()
+
+	for _, v := range me.Right {
+		rightQueries := v.Term.GetQueries()
+
+		queries = append(queries, rightQueries...)
+	}
+
+	queryMap := make(map[string]string)
+	for key, value := range queries {
+		queryMap[toCharStr(key+1)] = value
+	}
+	return queryMap
+}
+
+func toCharStr(i int) string {
+	return strings.ToLower(string('A' - 1 + i))
 }
 
 // NewMetricExpressionParser returns a Parser which is capable of interpretting
@@ -72,6 +97,61 @@ func NewMetricExpressionParser() *MetricExpressionParser {
 	}
 
 	return mep
+}
+
+// Display
+
+func (o Operator) String() string {
+	switch o {
+	case OpMul:
+		return "*"
+	case OpDiv:
+		return "/"
+	case OpSub:
+		return "-"
+	case OpAdd:
+		return "+"
+	}
+	panic("unsupported operator")
+}
+
+func (v *ExprValue) String() string {
+	if v.Number != nil {
+		return fmt.Sprintf("%g", *v.Number)
+	}
+	if v.MetricQuery != nil {
+		return v.MetricQuery.String()
+	}
+	return "(" + v.Subexpression.String() + ")"
+}
+
+func (f *Factor) String() string {
+	out := f.Base.String()
+	return out
+}
+
+func (o *OpFactor) String() string {
+	return fmt.Sprintf("%s %s", o.Operator, o.Factor)
+}
+
+func (t *Term) String() string {
+	out := []string{t.Left.String()}
+	for _, r := range t.Right {
+		out = append(out, r.String())
+	}
+	return strings.Join(out, " ")
+}
+
+func (o *OpTerm) String() string {
+	return fmt.Sprintf("%s %s", o.Operator, o.Term)
+}
+
+func (e *MetricExpression) String() string {
+	out := []string{e.Left.String()}
+	for _, r := range e.Right {
+		out = append(out, r.String())
+	}
+	return strings.Join(out, " ")
 }
 
 // MetricExpressionParser is parser returned when calling NewMetricExpressionParser.
